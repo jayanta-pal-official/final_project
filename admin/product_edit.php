b3be42c3b59de853f2e96c60ece0d2b28c438db9
jayanta-pal <paljayanta18@gmail.com>
2023-12-12 16:31:46 +0530

    show image before update

--- admin/product_edit.php
+++ admin/product_edit.php
@@ -21,68 +21,75 @@ include("include/common.php");
         </div><!-- /.container-fluid -->
     </div>
     <div class="content">
-            <div class="row">
-                <div class="col-md-12">
-                    <?php if (isset($_SESSION['status'])) {
-                        echo "<h4>" . $_SESSION['status'] . "</h4>";
-                    }
-                    unset($_SESSION['status']);
-                    ?>
-                    <div class="card">
-                        <div class="card-header">
-                            <h3 class="card-title">Edit - Product</h3>
-                            <a href="./product.php" class="btn btn-danger btn-sm  float-right"  >Back</a>
-                        </div>
-                        <!-- /.card-header -->
-                        <div class="card-body">
-                            <?php
-                            if (isset($_REQUEST['id'])) {
-                                $id = $_GET['id'];
-                                $sql = "SELECT * FROM `product` WHERE id=$id";
-                                $result = mysqli_query($conn, $sql);
-                                if (mysqli_num_rows($result) > 0) {
-                                    $row = mysqli_fetch_assoc($result); ?>
-                                    <div class="row">
-                                        <div class="col-md-6">
-                                            <form action="./p_code.php" method="POST" enctype="multipart/form-data" >
-                                                <input type="hidden" name="id" value="<?= $id ?>">
-                                                <div class="form-group">
-                                                    <label>Name</label>
-                                                    <input type="text" name="edit_name" class="form-control" placeholder="name" value="<?= $row['name']  ?>">
-                                                </div>
-                                                <div class="form-group">
-                                                    <label>Description </label>
-                                                    <input type="text" name="edit_description" class="form-control" placeholder="Email" value="<?= $row['description']  ?>">
-                                                </div>
-                                                <div class="form-group">
-                                                    <label>Price</label>
-                                                    <input type="text" name="edit_price" class="form-control" placeholder="Phone number" value="<?= $row['price']  ?>">
-                                                </div>
-                                                <div class="form-group">
+        <div class="row">
+            <div class="col-md-12">
+                <?php if (isset($_SESSION['status'])) {
+                    echo "<h4>" . $_SESSION['status'] . "</h4>";
+                }
+                unset($_SESSION['status']);
+                ?>
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Edit - Product</h3>
+                        <a href="./product.php" class="btn btn-danger btn-sm  float-right">Back</a>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                        <?php
+                        if (isset($_REQUEST['id'])) {
+                            $id = $_GET['id'];
+                            $sql = "SELECT * FROM `product` WHERE id=$id";
+                            $result = mysqli_query($conn, $sql);
+                            if (mysqli_num_rows($result) > 0) {
+                                $row = mysqli_fetch_assoc($result); ?>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <form action="./p_code.php" method="POST" enctype="multipart/form-data">
+                                            <input type="hidden" name="id" value="<?= $id ?>">
+                                            <div class="form-group">
+                                                <label>Name</label>
+                                                <input type="text" name="edit_name" class="form-control" placeholder="name" value="<?= $row['name']  ?>">
+                                            </div>
+                                            <div class="form-group">
+                                                <label>Description </label>
+                                                <input type="text" name="edit_description" class="form-control" placeholder="Email" value="<?= $row['description']  ?>">
+                                            </div>
+                                            <div class="form-group">
+                                                <label>Price</label>
+                                                <input type="text" name="edit_price" class="form-control" placeholder="Phone number" value="<?= $row['price']  ?>">
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-md-6">
                                                     <label>Image</label>
                                                     <input type="file" name="edit_image" id="image" class="form-control" placeholder="Image">
                                                 </div>
-                                                <input type="hidden"  name="id" value="<?php echo $_GET['id'] ?>">
-                                                <div class="modal-footer">
-                                                    <input type="submit" class="btn btn-primary" name="product_edit" value="Update">
+                                                <div class="col-md-6">
+                                                    <img src="<?php echo "../upload/" . $row['image'] ?>" alt="image" class="card-img-top" width="30px" height="60px" alt="...">
                                                 </div>
-                                        </div>
+                                                <!-- </div> -->
+                                            </div>
 
-                                        </form>
+                                            <input type="hidden" name="id" value="<?php echo $_GET['id'] ?>">
+                                            <div class="modal-footer">
+                                                <input type="submit" class="btn btn-primary" name="product_edit" value="Update">
+                                            </div>
                                     </div>
-                            <?php } else {
-                                    echo "<h1 style='color:red' >" . "No data in table" . "</h1>";
-                                }
+
+                                    </form>
+                                </div>
+                        <?php } else {
+                                echo "<h1 style='color:red' >" . "No data in table" . "</h1>";
                             }
+                        }
 
-                            ?>
+                        ?>
 
-                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 </div>
 </div>
 <?php
