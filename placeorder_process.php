8e68ab15b39bd536f127b921c01ac3831e9bdb9b
jayanta-pal <paljayanta18@gmail.com>
2023-12-18 03:12:31 +0530

    modify chack out page

--- placeorder_process.php
+++ placeorder_process.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include("admin/config/dbcon.php");
 
 if(isset($_REQUEST['placeorder'])){
@@ -8,18 +9,27 @@ if(isset($_REQUEST['placeorder'])){
     $city=$_REQUEST['city'];
     $state= $_REQUEST['state'];
     $zip =$_REQUEST['zip'];
+    
 
-    $product_id= $_REQUEST['product_id'];
-    $product_name = $_REQUEST['product_name'];
-    $product_image= $_REQUEST['product_image'];
-    $product_quentity = $_REQUEST['product_quentity'];
-    $product_price = $_REQUEST['product_price'];
-
-    $sql= "INSERT INTO order_details (name,email,address,city,State,pin,product_id,product_name,product_price,product_quentity,status) VALUES('$firstname', '$email','$address','$city','$state','$zip','$product_id', '$product_name', '$product_price','$product_quentity','0')";
+  $sql= "INSERT INTO order_details (name,email,address,city,State,pin) VALUES('$firstname', '$email','$address','$city','$state','$zip')";
     $result=mysqli_query($conn,$sql);
     if($result){
-        echo"<script>alert('save')</script>";
+        // echo "<script>alert('done')</script>";
+       header('location:order_confirm.php');
+       exit;
     }
 
+}
+if(isset($_REQUEST['placeorder'])){
+   
+        $product_id= $_REQUEST['product_id'];
+        $product_name = $_REQUEST['product_name'];
+        $product_image= $_REQUEST['product_image'];
+        $product_quentity = $_REQUEST['product_quentity'];
+        $product_price = $_REQUEST['product_price'];
+        $total_price= $_REQUEST['total_price'];
+        $_SESSION['order'][]=['product_id'=>$product_id, 'product_image'=>$product_image, 'product_quentity'=> $product_quentity, 'product_price'=>$product_price];
+
+    
 }
 ?>
