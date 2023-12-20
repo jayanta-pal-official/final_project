785acfec6f74d7d195b0629bb0a3c5c7cc3e73e0
jayanta-pal <paljayanta18@gmail.com>
2023-12-20 23:25:23 +0530

    fixed all reader send header problem

--- admin/user_delete.php
+++ admin/user_delete.php
@@ -8,10 +8,13 @@ include("include/common.php");
   $id = $_GET['id'];
   $delete_query = "DELETE FROM `user` WHERE id=$id";
   $delete_result = mysqli_query($conn, $delete_query);
-  if ($delete_query) {
+  if ($delete_result) {
     $_SESSION['status'] = "User deleted successfully";
-    header("location: registered.php");
-    exit;
+ echo "<script>
+ window.location='registered.php'
+ </script>";
+    // header("location: registered.php");
+    // exit;
   } else {
     $_SESSION['status'] = "User can't deleted ";
     header("location: registered.php");
