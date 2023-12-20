2b2ada2bac903baaec407ceafac2ad19062a6485
jayanta-pal <paljayanta18@gmail.com>
2023-12-20 19:43:39 +0530

    form validation for all input field's

--- admin/assets/dist/js/form.validation.js
+++ admin/assets/dist/js/form.validation.js
@@ -7,13 +7,54 @@ $(document).ready(function(){
             firstname:{
                 required: true,
                 minlength: 2,
-            
+            },
+            email:{
+                required: true,
+                email:true,
+            },
+            address:{
+                required: true,
+                minlength: 2,
+            },
+            city:{
+                required: true,
+                minlength: 2,
+            },
+            state:{
+                required: true,
+                minlength: 2,
+            },
+            zip:{
+                required: true,
+                minlength: 6,
+                number:true, 
             }
         },
         massage:{
             firstname:  {
                 required : "plese enter full name",
-                minlength: "full name should be character",
+                minlength: "full name should be two character",
+            },
+            email:  {
+                required : "plese enter email",
+                email: "invalid email Id",
+            },
+            address:  {
+                required : "plese enter address",
+                minlength: "address name should be two character",
+            },
+            city:  {
+                required : "plese enter city",
+                minlength: "city should be two character",
+            },
+            state:  {
+                required : "plese enter state",
+                minlength: "state should be two character",
+            },
+            zip:  {
+                required : "plese enter zip code",
+                minlength: "zip should be six digit",
+                number:"Invalid zip code",
             },
     
         },
