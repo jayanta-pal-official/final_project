7941a7b7ff6b4e9f20686b38bf72444e1393a575
jayanta-pal <paljayanta18@gmail.com>
2023-12-21 21:09:30 +0530

    insert cart items into order_details table

--- placeorder_process.php
+++ placeorder_process.php
@@ -1,7 +1,11 @@
 <?php
 session_start();
 include("admin/config/dbcon.php");
-
+if(isset($_SESSION['cart']))
+{
+  echo "<pre>";
+  print_r($_SESSION['cart']);
+}
 // if($_SESSION['u_loggedin'] == true){
 //   if(isset($_REQUEST['placeorder'])){
 //     $firstname= $_REQUEST['firstname'];
@@ -28,15 +32,21 @@ include("admin/config/dbcon.php");
   $sql= "INSERT INTO user (name,email,address,city,State,pin) VALUES('$firstname', '$email','$address','$city','$state','$zip')";
     $result=mysqli_query($conn,$sql);
     if($result){
+     
       $last_id = mysqli_insert_id($conn);
-      $product_id = $_REQUEST['product_id'];
-      $product_name= $_REQUEST['product_name'];
-      $product_quentity= $_REQUEST['product_quentity'];
-      $product_price= $_REQUEST['product_price'];
-      $total_price= $_REQUEST['total_price'];
+      if(isset($_SESSION['cart'])){
+        $total_price= $_REQUEST['total_price'];
+      }
+      foreach($_SESSION['cart'] as $key => $value){
+      $product_id = $key;
+      $product_name= $value['ProductName'];
+      $product_quentity= $value['quentity'];
+      $product_price= $value['ProductPrice'];
+      
       $sql_product_item = "INSERT INTO order_details (user_id,product_id,product_name,product_price,quantity,total_price)
-       VALUES('$last_id','$product_id','$product_name','$product_price','$product_quentity','$total_price')";
-     $result_item =mysqli_query($conn,$sql_product_item);
+      VALUES('$last_id','$product_id','$product_name','$product_price','$product_quentity','$total_price')";
+      $result_item =mysqli_query($conn,$sql_product_item);
+      }
      if($result_item){ ?>
       alert("Succesfully insert");
     <?php  }
